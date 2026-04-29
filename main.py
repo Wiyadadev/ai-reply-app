ddeaff851157869eb1a46e423e467221122c3037
Wiyadadev <wiyada281998@icloud.com>
2026-04-29 16:57:10 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8556254884:AAEU0VIclzFozyIHgo5dsMSdfB3m97vMVNI"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("บอททำงานแล้ว!")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
