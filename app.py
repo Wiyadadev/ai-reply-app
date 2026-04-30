e96af7a3ef5f88449bab0baa0ebf358804b711c3
Wiyadadev <wiyada281998@icloud.com>
2026-04-29 22:59:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify, send_from_directory
+_from flask import Flask, request, jsonify, send_from_directory
 from openai import OpenAI
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 client = OpenAI(api_key=os.environ.get("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"))
 
