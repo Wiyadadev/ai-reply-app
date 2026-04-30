b6ccbe6d9eb10cb7abb88af5276952d024ae1f20
Wiyadadev <wiyada281998@icloud.com>
2026-04-29 19:05:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=[GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     response = None
 
-    if request. method == 'POST':
+    if request.method == 'POST':
         user_input = request.form.get('user_input')
-        response = "You said: "+ user_input
+        response = "You said: " + user_input
 
-    return runder_template('index.html', response=response)
+    return render_template('index.html', response=response)
 
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run()
-  
