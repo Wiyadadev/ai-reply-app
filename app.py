56b9c8675840de0dbb28f8306d23669102a01bc8
Wiyadadev <wiyada281998@icloud.com>
2026-04-29 18:07:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=[GET', 'POST'])
 def home():
-    return "Hello from Python!"
+    response = None
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    if request. method == 'POST':
+        user_input = request.form.get('user_input')
+        response = "You said: "+ user_input
+
+    return runder_template('index.html', response=response)
+
+if __name__== '__main__':
+    app.run()
+  
