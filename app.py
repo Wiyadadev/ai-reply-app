bf00c55ab529986624f529b14676ff18b0e0e9e0
Wiyadadev <wiyada281998@icloud.com>
2026-05-01 07:16:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name_-)
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -13,5 +13,5 @@ def home():
 
     return render_template("index.html", result=result)
 
-if _-name_-== "_main_":
+if __name__== "_main_":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
