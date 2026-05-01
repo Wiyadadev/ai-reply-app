81fcb153e5c88adf7cc59198f022d045ded91035
Wiyadadev <wiyada281998@icloud.com>
2026-05-01 12:55:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.getcwd(), "templates"))
 
 @app.route("/")
 def home():
