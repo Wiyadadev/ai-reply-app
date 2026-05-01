6b6c84d6e15d4f1ced7d382e2b3b5dff870a0a2c
Wiyadadev <wiyada281998@icloud.com>
2026-05-01 12:20:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     result = None
     
     if request.method == "POST":
-        text = request.form.get(text)
+        text = request.from.get("text") or " "
 
         try:
             response = client.chat.completions.create(
