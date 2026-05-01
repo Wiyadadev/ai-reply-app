93b70a8b5a6db31b7cb860e783ba227f063d6479
Wiyadadev <wiyada281998@icloud.com>
2026-05-01 11:27:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,19 @@ def home():
     
     if request.method == "POST":
         text = request.form.get("text")
-        
-        response = client.chat.completions.create(
-            model="gpt-40-mini",
-            messages=[{"role": "user", "content": "text"}]
-        )
 
-        result= response.choices[0].message.content
+            try:
+                response = client.chat.completions.create(
+                    model="gpt-40-mini",
+                    messages=[{"role": "user", "content": text}]
+                )
+
+                result= response.choices[0].message.content
+    except Exception as e:
+        result = str(e)
 
     return render_template("index.html", result=result)
-except Exception as e:
-result = str(e)
+
 
 if __name__== "_main_":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
